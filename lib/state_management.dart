1b7a3a10fb885c5c736d2b1a0a88e9da6f9fb6a9
bullet_bane <tolegena1@gmail.com>
2021-02-20 19:30:17 +0600

    add: add getters to EditorProvider

--- lib/state_management.dart
+++ lib/state_management.dart
@@ -2,10 +2,21 @@ import 'package:flutter/material.dart';
 import 'text_field.dart';
 
 class EditorProvider extends ChangeNotifier {
+  List<FocusNode> _nodes = [];
+  List<TextEditingController> _text = [];
+  List<SmartTextType> _types = [];
   SmartTextType selectedType;
 
-  EditorProvider({SmartTextType defaultType = SmartTextType.T})
-      : selectedType = defaultType;
+  EditorProvider({SmartTextType defaultType = SmartTextType.T}) {
+    selectedType = defaultType;
+    insert(index: 0);
+  }
+
+  int get length => _text.length;
+  int get focus => _nodes.indexWhere((node) => node.hasFocus);
+  FocusNode nodeAt(int index) => _nodes.elementAt(index);
+  TextEditingController textAt(int index) => _text.elementAt(index);
+  SmartTextType typeAt(int index) => _types.elementAt(index);
 
   void setType(SmartTextType type) {
     if (selectedType == type) {
@@ -13,6 +24,22 @@ class EditorProvider extends ChangeNotifier {
     } else {
       selectedType = type;
     }
+    _types.removeAt(focus);
+    _types.insert(focus, selectedType);
     notifyListeners();
+  }
+
+  void setFocus(SmartTextType type) {
+    selectedType = type;
+    notifyListeners();
+  }
+
+  void insert({int index, String text, SmartTextType type = SmartTextType.T}) {
+    final TextEditingController controller =
+        TextEditingController(text: text ?? '');
+    controller.addListener(() {});
+    _text.insert(index, controller);
+    _types.insert(index, type);
+    _nodes.insert(index, FocusNode());
   }
 }
